ef6ec35d45e26725e78f415dd00f92fa61f300b0
Ryuichi Nonaka <nonaka.ryuichi@classmethod.jp>
2012-11-09 17:07:19 +0900

    add config test

---
+++ config.rb
@@ -0,0 +1,7 @@
+additional_import_paths = 'import/'
+http_path = 'http://classmethod.jp'
+#relative_assets = true
+http_stylesheets_dir = 'css/'
+http_stylesheets_path = 'http://dev.classmethod.jp/stylesheet/'
+http_images_dir = 'images/'
+#http_images_path = 'http://dev.classmethod.jp/img/'
